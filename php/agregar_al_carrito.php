70c56c7f29ec7638b838a85de80017a1fa09e2a4
USER <Jos Hernandez>
2023-06-18 00:31:52 -0500

    Validar la sesion al agregar un producto al carrito

--- php/agregar_al_carrito.php
+++ php/agregar_al_carrito.php
@@ -3,22 +3,30 @@
 require_once('conexion.php');
 $conn=Db::conectar();
 
-// verificar si se recibió el formulario con la información del artículo
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $usuario = $_POST['nombre']; 
-    $cveArticulo = $_POST['cveArticulo'];
-    $precio = $_POST['precio'];
-    $cantidad = $_POST['cantidad'];
+// Inicia sesion
+session_start();
 
-    $sql = "INSERT INTO carrito (cveArticulo, cantidadProducto, precio) VALUES ('$cveArticulo', '$cantidad', '$precio')";
-    if ($conn && $conn->query($sql)) {
-        // echo 'Artículo agregado al carrito con éxito';
-        header('Location: mostrar.usuario.php');
-    } else {
-        // echo 'Error al agregar el artículo al carrito: ' ;
-        echo '<script>alert(Error al agregar el artículo al carrito");</script>';
-    }
-} //else {
-    // echo 'No se recibió la información del artículo';
-// }
+// Valida si existe una sesion iniciada
+if (empty($_SESSION['correo'])) {
+	echo "<script> alert('Iniciar Sesion Por favor'); window.location='sesion.php' </script>";
+} else {
+	// verificar si se recibió el formulario con la información del artículo
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$correo = $_SESSION['correo']; // obtiene el correo de la sesion 
+		$cveArticulo = $_POST['cveArticulo']; // obtiene la clase del articulo
+		$cantidad = $_POST['cantidad']; // obtiene la cantidad de los articulos
+		$precio = $_POST['precio']; // obtiene el precio del articulo
+		$subTotal = $cantidad * $precio; // calcula el subtotal multiplicando la cantidad por el precio
+
+		$sql = "INSERT INTO carrito (correo, cveArticulo, cantidadProducto, precio, subtotal) VALUES ('$correo','$cveArticulo', '$cantidad', '$precio', '$subTotal')";
+		if ($conn && $conn->query($sql)) {
+			// echo 'Artículo agregado al carrito con éxito';
+			header('Location: mostrar.usuario.php');
+		} else {
+			// echo 'Error al agregar el artículo al carrito: ' ;
+			echo '<script>alert(Error al agregar el artículo al carrito");</script>';
+		}
+	}
+}
+
 ?>
