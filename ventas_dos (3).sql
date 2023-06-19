b753ec09e0d1218f00ad79e95941a4477b51f13e
USER <Jos Hernandez>
2023-06-19 08:41:11 -0500

    actualizacion de sql

--- ventas_dos (3).sql
+++ ventas_dos (3).sql
@@ -7,16 +7,15 @@ CREATE DATABASE IF NOT EXISTS `ventas_dos`;
 USE `ventas_dos`;
 
 -- --------------------------------------------------------
-
 --
 -- Estructura de tabla para la tabla `articulos`
 --
 
-CREATE TABLE IF NOT EXISTS `articulos` (
+CREATE TABLE `articulos` (
   `cveArticulo` varchar(50) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `precio` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `articulos`
@@ -34,14 +33,14 @@ INSERT INTO `articulos` (`cveArticulo`, `nombre`, `precio`) VALUES
 -- Estructura de tabla para la tabla `carrito`
 --
 
-CREATE TABLE IF NOT EXISTS `carrito` (
+CREATE TABLE `carrito` (
   `id_compra` int(50) NOT NULL,
   `correo` varchar(50) DEFAULT NULL,
   `cveArticulo` varchar(50) NOT NULL,
   `cantidadProducto` int(7) NOT NULL,
   `precio` float NOT NULL,
   `subtotal` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `carrito`
@@ -54,7 +53,21 @@ INSERT INTO `carrito` (`id_compra`, `correo`, `cveArticulo`, `cantidadProducto`,
 (6, NULL, '3', 1, 656, 0),
 (7, NULL, '01', 1, 32, 0),
 (8, NULL, '8', 1, 10, 0),
-(9, NULL, '8', 7, 10, 0);
+(9, NULL, '8', 7, 10, 0),
+(10, NULL, '01', 1, 32, 0),
+(11, NULL, '2', 3, 28, 0),
+(12, NULL, '01', 1, 32, 0),
+(13, NULL, '01', 1, 32, 0),
+(14, 'jose@gmail.com', '8', 4, 10, 0),
+(15, 'jose@gmail.com', '01', 3, 32, 96),
+(16, 'alfredo@gmail.com', '01', 3, 32, 96),
+(17, 'alfredo@gmail.com', '8', 5, 10, 50),
+(18, 'jose@gmail.com', '01', 2, 32, 64),
+(19, '4@gmail.com', '2', 5, 28, 140),
+(20, NULL, '01', 1, 32, 32),
+(21, 'lulu@gmail.com', '01', 2, 32, 64),
+(22, 'lulu@gmail.com', '2', 3, 28, 84),
+(23, 'lulu@gmail.com', '8', 5, 10, 50);
 
 -- --------------------------------------------------------
 
@@ -62,7 +75,7 @@ INSERT INTO `carrito` (`id_compra`, `correo`, `cveArticulo`, `cantidadProducto`,
 -- Estructura de tabla para la tabla `detalleventa`
 --
 
-CREATE TABLE IF NOT EXISTS `detalleventa` (
+CREATE TABLE `detalleventa` (
   `claveDetalle` int(50) NOT NULL,
   `claveVenta` int(50) NOT NULL,
   `correoUsuario` varchar(50) NOT NULL,
@@ -72,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `detalleventa` (
   `subtotal` double NOT NULL,
   `iva` double NOT NULL,
   `total` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -80,15 +93,16 @@ CREATE TABLE IF NOT EXISTS `detalleventa` (
 -- Estructura de tabla para la tabla `pago`
 --
 
-CREATE TABLE IF NOT EXISTS `pago` (
-  `clavePago` int(50) NOT NULL,
-  `opcionPago` varchar(50) NOT NULL,
-  `cuentaBancaria` varchar(50) NOT NULL,
-  `fechaVencimiento` varchar(50) NOT NULL,
-  `cvv` int(50) NOT NULL,
-  `correoUsuario` varchar(50) NOT NULL,
-  `nombreUsuario` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `pago` (
+  `clavePago` int(11) NOT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `telefono` varchar(30) DEFAULT NULL,
+  `numerotarjeta` varchar(30) DEFAULT NULL,
+  `fechaexpiracion` varchar(30) DEFAULT NULL,
+  `cvv` varchar(30) DEFAULT NULL,
+  `importe` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -96,11 +110,11 @@ CREATE TABLE IF NOT EXISTS `pago` (
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE IF NOT EXISTS `usuarios` (
+CREATE TABLE `usuarios` (
   `correo` varchar(50) NOT NULL,
   `contrasena` varchar(50) NOT NULL,
   `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -110,7 +124,14 @@ INSERT INTO `usuarios` (`correo`, `contrasena`, `nombre`) VALUES
 ('2@gmail.com', '1', ''),
 ('3@gmail.com', '1', ''),
 ('4@gmail.com', '1', ''),
-('jose@gmail.com', '321', 'Jose');
+('admin@gmail.com', 'admin', 'administrador'),
+('alfredo@gmail.com', 'jose', ''),
+('jose@gmail.com', '321', 'Jose'),
+('lulu@gmail.com', 'lulu', 'lulu'),
+('mian@gmail.com', 'mian', 'mian'),
+('sandra@gmail.com', 'sandra', 'sandra'),
+('tommy@gmail.com', 'tommy', 'tommy'),
+('wolf@gmail.com', 'wolf', 'wolf');
 
 -- --------------------------------------------------------
 
@@ -118,13 +139,22 @@ INSERT INTO `usuarios` (`correo`, `contrasena`, `nombre`) VALUES
 -- Estructura de tabla para la tabla `venta`
 --
 
-CREATE TABLE IF NOT EXISTS `venta` (
+CREATE TABLE `venta` (
   `claveVenta` int(50) NOT NULL,
-  `cveArticulo` varchar(50) NOT NULL,
-  `cantidadProducto` int(11) NOT NULL,
-  `subtotal` float NOT NULL,
-  `total` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `usuario` varchar(50) DEFAULT NULL,
+  `cantidadProducto` int(11) DEFAULT NULL,
+  `total` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`claveVenta`, `usuario`, `cantidadProducto`, `total`) VALUES
+(1, 'jose@gmail.com', 9, 232),
+(6, 'lulu@gmail.com', 10, 229.68),
+(7, 'lulu@gmail.com', 10, 229.68),
+(8, 'lulu@gmail.com', 10, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -143,6 +173,18 @@ ALTER TABLE `carrito`
   ADD UNIQUE KEY `id_compra` (`id_compra`),
   ADD KEY `correo` (`correo`),
   ADD KEY `cveArticulo` (`cveArticulo`);
+
+--
+-- Indices de la tabla `detalleventa`
+--
+ALTER TABLE `detalleventa`
+  ADD PRIMARY KEY (`claveDetalle`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`clavePago`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -164,13 +206,25 @@ ALTER TABLE `venta`
 -- AUTO_INCREMENT de la tabla `carrito`
 --
 ALTER TABLE `carrito`
-  MODIFY `id_compra` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_compra` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de la tabla `detalleventa`
+--
+ALTER TABLE `detalleventa`
+  MODIFY `claveDetalle` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `clavePago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
-  MODIFY `claveVenta` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `claveVenta` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
@@ -181,4 +235,3 @@ ALTER TABLE `venta`
 --
 ALTER TABLE `carrito`
   ADD CONSTRAINT `carrito_ibfk_2` FOREIGN KEY (`cveArticulo`) REFERENCES `articulos` (`cveArticulo`) ON DELETE CASCADE ON UPDATE CASCADE;
-
